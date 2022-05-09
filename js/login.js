72644def0efda459f3ba3f44d9c268d161a9f5fa
CesarFrutosX <cesarfrutos_x@yahoo.es>
2022-05-09 13:56:17 +0200

    prueba vscode

---
+++ js/login.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCBXTTknGr0bzDOwMZpjawbyDlxNrj7tig",
+    authDomain: "proyecto-final-lm.firebaseapp.com",
+    projectId: "proyecto-final-lm",
+    storageBucket: "proyecto-final-lm.appspot.com",
+    messagingSenderId: "805432992319",
+    appId: "1:805432992319:web:4f69bb31378dfae6e60e8a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
