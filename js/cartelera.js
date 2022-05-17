7198e3985df860fbae5035125e47d988e3c29be9
cfrutoss <cesarfrutos_x@yahoo.es>
2022-05-17 17:02:57 +0200

    cartelera recogida (bien)

--- js/cartelera.js
+++ js/cartelera.js
@@ -11,9 +11,33 @@ window.addEventListener ('DOMContentLoaded', async (event)  => {
     moviesList.forEach((doc) => {
         console.log(doc);
         let movie = doc.data();
-        
+        getCuadro(movie);
 
     });
 });
 
+function getCuadro(movie){
+    let marcoCuadros = document.getElementById("marcoCartelera");
+
+    marcoCuadros.innerHTML += `<div class="cuadro"> 
+                                <div class="imgPeli">
+                                    <img src="${movie.imgurl}">
+                                </div> 
+                                <div class="contentPeli">
+                                <h2>${movie.title}</h2>
+                                ${movie.description}
+                                </div>
+                                <div class="botonydiv"> 
+                                    <div class="space"> </div>
+                                    <a href="Reserva.html"> 
+                                    <button class="botonreserva"> 
+                                        <span class="textobtn">Reservar</span>
+                                        <span class="iconitobtn"> 
+                                            <ion-icon name="chevron-forward-outline">
+                                            </ion-icon> 
+                                        </span> 
+                                    </button>
+                                    </a> 
+                                </div>`
+}
 
