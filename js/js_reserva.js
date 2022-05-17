36c6a3aa0afc07b7e9b31c4b40d1af206ac70cf5
alapatacoja <medaigualsihackean@gmail.com>
2022-05-17 17:20:32 +0200

    plano asientos hecho

--- js/js_reserva.js
+++ js/js_reserva.js
@@ -1,2 +1,12 @@
-const seats = document.querySelectorAll('.fila .asiento:not(.reservado');
+const asientos = document.querySelectorAll('.fila .asiento:not(.reservado');
+const mapa = document.querySelector('.mapa');
+
+
+mapa.addEventListener('click', (e) => {
+  if (e.target.classList.contains('asiento') && !e.target.classList.contains('reservado')) {
+    e.target.classList.toggle('seleccionado');
+
+    updateSelectedCount();
+  }
+});
 
