bfbe7ec74b3826d466686251a57673c7951d9619
alapatacoja <medaigualsihackean@gmail.com>
2022-05-19 19:28:40 +0200

    nada importante

--- js/js_reserva.js
+++ js/js_reserva.js
@@ -1,3 +1,5 @@
+// import { jsPDF } from "jspdf";
+
 const asientos = document.querySelectorAll('.fila .asiento:not(.reservado');
 const mapa = document.querySelector('.mapa');
 
@@ -10,6 +12,21 @@ mapa.addEventListener('click', (e) => {
   }
 });
 
+// function generarPDF(){
+//   /* variables input */
+//  // var titulo = ????;
+//   var inombre = document.getElementById('inombre').value;
+//   var iapellidos = document.getElementById('iapellidos').value;
+//   var iemail = document.getElementById('iemail').value;
+//
+//   var doc = new jsPDF();
+//
+//   doc.setFontSize(18);
+//   doc.text('nombre: '+inombre, 10, 20);
+//   doc.text('apellidos: '+iapellidos, 10, 20);
+//   doc.text('email: '+iemail, 10, 20);
+//
+// }
 function updateSelectedCount(){
   const asientosSeleccionados = document.querySelectorAll('.fila .asiento.seleccionado');
 
