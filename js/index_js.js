191f9b9164336f6a035c17272a28919b21dbfc4d
alapatacoja <medaigualsihackean@gmail.com>
2022-05-28 09:44:36 +0200

    inicio y poco mas

--- js/index_js.js
+++ js/index_js.js
@@ -2,7 +2,7 @@ var counter = 1;
 setInterval(function (){
     document.getElementById('radio'+counter).checked = true;
     counter++;
-    if (counter>4){
+    if (counter>5){
       counter=1;
     }
-}, 5000);
+}, 5000); 
--- js/js_general.js
+++ js/js_general.js
@@ -18,6 +18,8 @@
   } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
 
   import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword  } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js';;
+  //import { Link } from 'react-router-dom';
+
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
@@ -172,3 +174,24 @@ export const addReserva = async (titulo,dia,hora,asientos) => {
     
 }
 
+
+//https://stackoverflow.com/questions/70700322/how-do-i-update-my-navbar-once-a-user-is-logged-in-from-log-in-to-profile
+
+
+/*HEADER*/
+/*
+const HEADER = () => {
+    return <header id="header">
+                <Link to="/Index.html" class="logo">
+                    <img src="imgs/ELMOCINES.png" alt="logo" class="logo" onmouseover="hover(this)" onmouseout="unhover(this)"/>
+                </Link>
+                <ul class="menu">
+                    <li class="1"> <Link to="/Index.html" class="item">inicio</Link></li>
+                    <li class="2"> <Link to="/Cartelera.html" class="item">cartelera</Link></li>
+                    {user && <Link to="/ListaReservas.html" class="item">Reservas</Link>}
+                    {!user && <Link to="/Usuarios.html" class="item">Usuarios</Link>}
+                    <Link to="/Login.html"><li class="login-header" id="logger">login</li></Link>
+                </ul>
+            </header>
+}
+*/
